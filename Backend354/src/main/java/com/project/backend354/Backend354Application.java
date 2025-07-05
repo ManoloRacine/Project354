c5148937ed12abf4de6181bd93a967f3bfb14b23
ManoloRacine <manolo191202@gmail.com>
2025-07-04 22:08:35 -0400

    initial commit

---
+++ Backend354/src/main/java/com/project/backend354/Backend354Application.java
@@ -0,0 +1,13 @@
+package com.project.backend354;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend354Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend354Application.class, args);
+	}
+
+}
---
+++ Backend354/src/test/java/com/project/backend354/Backend354ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backend354;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend354ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
