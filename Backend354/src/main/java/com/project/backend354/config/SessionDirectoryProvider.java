cf6ab44b97ec496f9a15b415addfccba3255497b
Mark Antabi <antabi39@gmail.com>
2025-07-07 20:49:06 -0400

    Add directory provider for image storage service

---
+++ Backend354/src/main/java/com/project/backend354/config/SessionDirectoryProvider.java
@@ -0,0 +1,47 @@
+package com.project.backend354.config;
+
+import jakarta.annotation.PreDestroy;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
+@SessionScope
+public class SessionDirectoryProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(SessionDirectoryProvider.class);
+
+    private final Path sessionDir;
+
+    public SessionDirectoryProvider(
+            @Value("${upload.base-dir}") String uploadRoot,
+            HttpSession session) throws IOException {
+
+        this.sessionDir = Paths.get(uploadRoot, session.getId());
+        Files.createDirectories(sessionDir);
+        log.info("Session upload dir created: {}", sessionDir);
+    }
+
+    public Path getDirectory() {
+        return sessionDir;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        try {
+            FileSystemUtils.deleteRecursively(sessionDir);
+            log.info("Session upload dir deleted: {}", sessionDir);
+        } catch (IOException e) {
+            log.error("Failed to delete session dir: {}", sessionDir, e);
+        }
+    }
+}
